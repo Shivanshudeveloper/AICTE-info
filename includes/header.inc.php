4d5a89dd5ae9e2d1ce5b71232ee809033284ee17
Shivanshu981 <developer.shivanshu@gmail.com>
2020-04-08 21:55:46 -0700

    AICTE Information

---
+++ includes/header.inc.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AICTE</title>
+</head>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+<!-- JQuery -->
+<script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+<script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+<link href="https://fonts.googleapis.com/css2?family=Comic+Neue:wght@700&family=Roboto:wght@500&display=swap" rel="stylesheet"> 
+
+<style>
+    h1, h2, h3, h4 {
+        font-family: 'Comic Neue', cursive;
+        font-family: 'Roboto', sans-serif;
+    }
+</style>
+
+<body>
+
+<section class="mt-4">
+    <center>
+        <img style="width: 10%;" class="img img-fluid" src="https://upload.wikimedia.org/wikipedia/en/e/eb/All_India_Council_for_Technical_Education_logo.png" alt="" srcset="">
+    </center>
+</section>
---
+++ index.php
@@ -0,0 +1,251 @@
+<?php include './includes/header.inc.php' ?>
+
+<div class="container mt-4">
+    <form action="#!" method="POST">
+        <div class="row">
+            <div class="col">
+                <div class="form-group">
+                    <label for="exampleFormControlSelect1">Select Stream</label>
+                    <select class="form-control" id="exampleFormControlSelect1">
+                        <option>B-tech</option>
+                        <option>B-Com</option>
+                        <option>M-tech</option>
+                        <option>Bse</option>
+                        <option>B-Pharma</option>
+                    </select>
+                </div>
+            </div>
+
+            <div class="col">
+                <div class="form-group">
+                    <label for="exampleFormControlSelect1">Select Branch</label>
+                    <select class="form-control" id="exampleFormControlSelect1">
+                        <option>Computer Science</option>
+                        <option>Arts</option>
+                        <option>Humanitites</option>
+                        <option>Commerce</option>
+                        <option>Sports</option>
+                    </select>
+                </div>
+            </div>
+
+            <div class="col">
+                <div class="form-group">
+                    <label for="exampleFormControlSelect1">Select Year</label>
+                    <select class="form-control" id="exampleFormControlSelect1">
+                        <option>1 Year</option>
+                        <option>2 Year</option>
+                        <option>3 Year</option>
+                        <option>4 Year</option>
+                    </select>
+                </div>
+            </div>
+
+            <div class="col">
+                <div class="form-group">
+                    <label for="exampleFormControlSelect1">Select Subject</label>
+                    <select class="form-control" id="exampleFormControlSelect1">
+                        <option>Computer Network</option>
+                        <option>Data Structure</option>
+                        <option>Algorithms</option>
+                        <option>Neural Science</option>
+                    </select>
+                </div>
+            </div>
+        </div>
+
+        <center>
+            <button class="btn btn-primary w-25">
+                Search
+            </button>
+        </center>
+    </form>
+
+<section class="mt-4 mb-4">
+    <div class="card rounded shadow-sm">
+        <div class="card-body">
+            <h1 class="card-title">Big Data Analytics</h1>
+            <h6 class="card-subtitle mb-2 text-muted">E-Contents</h6>
+            <hr>
+            <p class="card-text">
+                <h4 class="text-center">
+                    Contents from Swayam
+                </h4>
+                <div>
+                    <div class="row row-cols-1 row-cols-md-5">
+                        <div class="col mb-4">
+                        <div class="card border-0">
+                            <div class="card-body">
+                            <span class="h4">
+                                <a href="/viewnotes/Data Structures">
+                                        <i class="fas fa-file-video"></i>
+                                </a>
+                            </span>
+                            <h6>Data Structures</h3>
+                            </div>
+                        </div>
+                        </div>
+                        <div class="col mb-4">
+                            <div class="card border-0">
+                            <div class="card-body">
+                                <span class="h4">
+                                    <a href="#!">
+                                        <i class="fas fa-file-video"></i>
+                                    </a>
+                                </span>
+                                <h6>Algorithms</h3>
+                            </div>
+                            </div>
+                        </div>
+                        <div class="col mb-4">
+                            <div class="card border-0">
+                            <div class="card-body">
+                                <span class="h4">
+                                    <a href="#!">
+                                        <i class="fas fa-file-video"></i>
+                                    </a>
+                                </span>
+                                <h6>Compiler</h3>
+                            </div>
+                            </div>
+                        </div>
+
+                        <div class="col mb-4">
+                            <div class="card border-0">
+                            <div class="card-body">
+                                <span class="h4">
+                                    <a href="#!">
+                                        <i class="fas fa-file-video"></i>
+                                    </a>
+                                </span>
+                                <h6>Operating System</h6>
+                            </div>
+                            </div>
+                        </div>
+
+                        <div class="col mb-4">
+                            <div class="card border-0">
+                            <div class="card-body">
+                                <span class="h4">
+                                    <a href="#!">
+                                        <i class="fas fa-file-video"></i>
+                                    </a>
+                                </span>
+                                <h6>Networking</h6>
+                            </div>
+                            </div>
+                        </div>
+
+                        <div class="col mb-4">
+                            <div class="card border-0">
+                            <div class="card-body">
+                                <span class="h4">
+                                    <a href="#!">
+                                        <i class="fas fa-file-video"></i>
+                                    </a>
+                                </span>
+                                <h6>Compiler</h3>
+                            </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </p>
+
+
+            <hr>
+            <p class="card-text">
+                <h4 class="text-center">
+                    Contents from National Digital Library
+                </h4>
+                <div>
+                    <div class="row row-cols-1 row-cols-md-5">
+                        <div class="col mb-4">
+                        <div class="card border-0">
+                            <div class="card-body">
+                            <span class="h4">
+                                <a href="/viewnotes/Data Structures">
+                                        <i class="fas fa-book"></i>
+                                </a>
+                            </span>
+                            <h6>Data Structures</h3>
+                            </div>
+                        </div>
+                        </div>
+                        <div class="col mb-4">
+                            <div class="card border-0">
+                            <div class="card-body">
+                                <span class="h4">
+                                    <a href="#!">
+                                        <i class="fas fa-book"></i>
+                                    </a>
+                                </span>
+                                <h6>Algorithms</h3>
+                            </div>
+                            </div>
+                        </div>
+                        <div class="col mb-4">
+                            <div class="card border-0">
+                            <div class="card-body">
+                                <span class="h4">
+                                    <a href="#!">
+                                        <i class="fas fa-book"></i>
+                                    </a>
+                                </span>
+                                <h6>Compiler</h3>
+                            </div>
+                            </div>
+                        </div>
+
+                        <div class="col mb-4">
+                            <div class="card border-0">
+                            <div class="card-body">
+                                <span class="h4">
+                                    <a href="#!">
+                                        <i class="fas fa-book"></i>
+                                    </a>
+                                </span>
+                                <h6>Operating System</h6>
+                            </div>
+                            </div>
+                        </div>
+
+                        <div class="col mb-4">
+                            <div class="card border-0">
+                            <div class="card-body">
+                                <span class="h4">
+                                    <a href="#!">
+                                        <i class="fas fa-book"></i>
+                                    </a>
+                                </span>
+                                <h6>Networking</h6>
+                            </div>
+                            </div>
+                        </div>
+
+                        <div class="col mb-4">
+                            <div class="card border-0">
+                            <div class="card-body">
+                                <span class="h4">
+                                    <a href="#!">
+                                        <i class="fas fa-book"></i>
+                                    </a>
+                                </span>
+                                <h6>Compiler</h3>
+                            </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </p>
+        </div>
+    </div>
+</section>
+
+</div>
+<!-- Container End -->
+
+
+<?php include './includes/footer.inc.php' ?>
